ce9569f4da53b4f1f5bdb05f6cc2e687b5510085
lis154 <lis154@mail.ru>
2020-10-03 21:36:48 +0300

    commit7

--- src/main/java/ru/lis154/SpringBootSecPr/controller/UserController.java
+++ src/main/java/ru/lis154/SpringBootSecPr/controller/UserController.java
@@ -28,13 +28,13 @@ public class UserController {
     }
 
     @RequestMapping(value = "/admin", method = RequestMethod.GET)
-        public String allUser(@RequestParam(defaultValue = "1") int page, Model model){
+    public String allUser(@RequestParam(defaultValue = "1") int page, Model model) {
         List<User> listUser = userService.allUser(page);
         int userCount = userService.userCount();
         int pageCout = (userCount + 9) / 10;
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         String nameUser = auth.getName();
-        User user1 = (User)userService.loadUserByUsername(nameUser);
+        User user1 = (User) userService.loadUserByUsername(nameUser);
         String roles = user1.getListRoles();
         model.addAttribute("users", listUser);
         model.addAttribute("name", nameUser);
@@ -42,25 +42,25 @@ public class UserController {
 
         System.out.println(listUser);
 
-         return "admin";
+        return "admin";
     }
 
     @RequestMapping(value = "/user", method = RequestMethod.GET)
-    public String OneUser( Model model){
+    public String OneUser(Model model) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         String nameUser = auth.getName();
-        User user1 = (User)userService.loadUserByUsername(nameUser);
-      //  User listUser = (User) userService.loadUserByUsername(name);
-       // model.addAttribute("user", listUser);
+        User user1 = (User) userService.loadUserByUsername(nameUser);
+        //  User listUser = (User) userService.loadUserByUsername(name);
+        // model.addAttribute("user", listUser);
         model.addAttribute("user", user1);
         return "user";
     }
 
     @RequestMapping(value = "/admin/add", method = RequestMethod.GET)
-    public String UserADD( Model model){
+    public String UserADD(Model model) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         String nameUser = auth.getName();
-        User user1 = (User)userService.loadUserByUsername(nameUser);
+        User user1 = (User) userService.loadUserByUsername(nameUser);
         String roles = user1.getListRoles();
         model.addAttribute("name", nameUser);
         model.addAttribute("role", roles);
@@ -68,7 +68,7 @@ public class UserController {
     }
 
     @RequestMapping(value = "/admin/add", method = RequestMethod.POST)
-    public String AddUserADD(@RequestParam String name, @RequestParam String password, @RequestParam String age, @RequestParam String role,  Model model){
+    public String AddUserADD(@RequestParam String name, @RequestParam String password, @RequestParam String age, @RequestParam String role, Model model) {
         int age1 = Integer.valueOf(age);
         User user = new User(name, password, age1);
         user.setRolesOnForm(role);
@@ -76,22 +76,29 @@ public class UserController {
         userService.add(user);
         return "redirect:/admin";
     }
-    @RequestMapping(value = "/admin/delete/{id}", method = RequestMethod.GET)
-    public String delete(@PathVariable("id") int id, Model model) {
-        userService.delete(id);
+
+    //    @RequestMapping(value = "/admin/delete/{id}", method = RequestMethod.GET)
+//    public String delete(@PathVariable("id") int id, Model model) {
+//        userService.delete(id);
+//        return "redirect:/admin";
+//    }
+    @RequestMapping(value = "/admin/delete", method = RequestMethod.POST)
+    public String delete(@RequestParam String id, Model model) {
+        int id1 = Integer.valueOf(id);
+        userService.delete(id1);
         return "redirect:/admin";
     }
 
     @RequestMapping(value = "/admin/edit/{id}", method = RequestMethod.GET)
-    public String UserEdit(@PathVariable("id") String id,   Model model){
+    public String UserEdit(@PathVariable("id") String id, Model model) {
         model.addAttribute("id", id);
         return "edit";
     }
 
     @RequestMapping(value = "/admin/edit", method = RequestMethod.POST)
-    public String EdUserEdit(@RequestParam String id, @RequestParam String name, @RequestParam String password, @RequestParam String age, @RequestParam String role,  Model model){
+    public String EdUserEdit(@RequestParam String id, @RequestParam String name, @RequestParam String password, @RequestParam String age, @RequestParam String role, Model model) {
         int age1 = Integer.valueOf(age);
-        int id1 =  Integer.valueOf(id);
+        int id1 = Integer.valueOf(id);
         User user = new User(id1, name, password, age1);
         user.setRolesOnForm(role);
         System.out.println(user);
@@ -107,8 +114,6 @@ public class UserController {
         }
         return "redirect:/";
     }
-
-
 
 
 }
